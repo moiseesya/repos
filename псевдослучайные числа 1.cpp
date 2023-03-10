61977b7c61f9fc73b706a00c86812de121859a48
moiseesya <121014855+moiseesya@users.noreply.github.com>
2023-03-10 17:04:15 +1000

    Add files via upload

---
+++ псевдослучайные числа 1.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+main()
+{
+	int mas[100][100];
+	
+	int a,b;
+	
+	printf("a=");
+	scanf("%i",&a);
+	
+    printf("b=");
+	scanf("%i",&b);
+	
+	srand(time(NULL));
+	
+	for(int j=0;j<a;j++)
+	{
+	
+    	for(int i=0;i<b;i++)
+	    {
+		mas[j][i]=rand()%10+(-2);
+	    }
+    }
+    for(int j=0;j<a;j++)
+	{
+	
+    	for(int i=0;i<b;i++)
+	    {
+			printf("%7i",mas[j][i]);
+	    }
+	    printf("\n\n");
+    }
+
+}
